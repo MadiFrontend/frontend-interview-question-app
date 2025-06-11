dac16f1c47701a48598a20c50309c65f8db2a630
madidev <madifrontend@gmail.com>
2025-06-11 15:49:51 +0330

    fix layout and style tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 export const metadata: Metadata = {
   title: "HR Interview Questions",
@@ -15,34 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased bg-secondary text-gray-900`}>
-        {children}
+      <body
+        className={` bg-secondary grid min-h-screen grid-rows-[80px_1fr_auto] text-gray-900`}
+      >
+        <Header />
+        <main className="container mx-auto px-4 py-8">{children}</main>
+        <Footer />
       </body>
     </html>
   );
 }
-
-// import { Inter } from 'next/font/google';
-// import Header from '../components/Header';
-// import Footer from '../components/Footer';
-// import './globals.css';
-
-// const inter = Inter({ subsets: ['latin'] });
-
-// export const metadata = {
-//   title: 'HR Interview Questions',
-//   description: 'A collection of HR interview questions and answers for front-end developers',
-//   keywords: 'HR, interview, questions, front-end, preparation',
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body className={`${inter.className} bg-secondary text-gray-900`}>
-//         <Header />
-//         <main className="container mx-auto px-4 py-8">{children}</main>
-//         <Footer />
-//       </body>
-//     </html>
-//   );
-// }
